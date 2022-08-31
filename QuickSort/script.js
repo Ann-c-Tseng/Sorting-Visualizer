3e2703a23c4e1ec11fba2a2dbb2d86e800aa385f
ann-c-tseng <ann.c.tseng@gmail.com>
2022-08-30 20:44:55 -0700

    added index to bars of QuickSort

--- QuickSort/script.js
+++ QuickSort/script.js
@@ -21,11 +21,40 @@ function makeArrayBlocks() {
     array_element_label.classList.add("block_id");
     array_element_label.innerText = value;
 
-    //Add the created elements to our html file.
+    //Add the created elements to html file.
     array_element.appendChild(array_element_label);
     container.appendChild(array_element);
   }
 }
 
+
+//Make the indices for each block
+var count_container = document.getElementById("count");
+function makeIdx(){
+    for(var i = 0; i < 20; i++){
+        //Make another array element div again for each index
+        var array_element2 = document.createElement("div");
+
+        //Add the class 'block2' to div
+        array_element2.classList.add("block2");
+
+        //Add height + transform style to div
+        array_element2.style.height = `${20}px`;
+        array_element2.style.transform = `translate(${i*30}px)`;
+
+        //Add the indices with a label element
+        var array_element_label2 = document.createElement("label");
+        array_element_label2.classList.add("block_id2");
+        array_element_label2.innerText = i;
+
+        //Add the created elements to html file
+        array_element2.appendChild(array_element_label2);
+        count_container.appendChild(array_element2);
+    }
+}
+
 // Call to generate the array
 makeArrayBlocks();
+
+// Call to generate the indices
+makeIdx();
